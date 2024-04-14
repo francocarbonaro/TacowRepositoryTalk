2a7493f9f1961e863272368ef3a4e14094310731
Franco Carbonaro <franco2610@gmail.com>
2024-04-14 15:34:56 -0400

    Initial Commit

---
+++ TacowRepository/TacowRepositoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TacowRepositoryApp.swift
+//  TacowRepository
+//
+//  Created by Franco Carbonaro on 2024-04-14.
+//
+
+import SwiftUI
+
+@main
+struct TacowRepositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
